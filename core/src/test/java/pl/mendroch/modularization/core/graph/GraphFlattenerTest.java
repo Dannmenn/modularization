930bef15fc5144641b131ddf2fa6f2cfc6c9751c
Daniel <daniel.mendroch@gmail.com>
2019-05-19 13:28:05 +0200

    additional test

--- core/src/test/java/pl/mendroch/modularization/core/graph/GraphFlattenerTest.java
+++ core/src/test/java/pl/mendroch/modularization/core/graph/GraphFlattenerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import pl.mendroch.modularization.common.api.model.tree.Node;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static pl.mendroch.modularization.common.api.model.tree.Node.node;
@@ -17,7 +18,7 @@ public class GraphFlattenerTest {
     }
 
     @Test
-    public void baseCase() {
+    public void baseGraph() {
         Node<String> d = node("d");
         Node<String> h = node("h", d);
         Node<String> g = node("g", h);
@@ -29,5 +30,27 @@ public class GraphFlattenerTest {
         GraphFlattener<String> flattener = new GraphFlattener<>(root);
 
         assertEquals(Arrays.asList("y", "x", "b", "c", "f", "u", "g", "z", "h", "d"), flattener.flatten());
+    }
+
+    @Test
+    public void complexGraph() {
+        Node<String> l = node("l");
+        Node<String> k = node("k", l);
+        Node<String> c = node("c");
+        Node<String> d = node("d", c, k);
+        Node<String> b = node("b", d, l);
+        Node<String> h = node("h", b);
+        Node<String> j = node("j", h);
+        Node<String> i = node("i", k, j);
+        Node<String> f = node("f", h, i);
+        Node<String> g = node("g", h);
+        Node<String> e = node("e", f, g);
+        Node<String> a = node("a", b, c, d, e);
+        Node<String> root = node("x", c, a, b, e);
+        GraphFlattener<String> flattener = new GraphFlattener<>(root);
+
+        List<String> result = flattener.flatten();
+        System.out.println(result);
+        assertEquals(Arrays.asList("x", "a", "e", "g", "f", "i", "j", "h", "b", "d", "c", "k", "l"), result);
     }
 }
